c5bdfb4285e7980c40725a21162eabdd3fab5bde
Philip <creativepyt@gmail.com>
2020-03-25 22:38:19 +0100

    Mini Navbar

---
+++ src/app/navbars/min-nav/min-nav.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MinNavComponent } from './min-nav.component';
+
+describe('MinNavComponent', () => {
+  let component: MinNavComponent;
+  let fixture: ComponentFixture<MinNavComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MinNavComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MinNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/navbars/min-nav/min-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-min-nav',
+  templateUrl: './min-nav.component.html',
+  styleUrls: ['./min-nav.component.scss']
+})
+export class MinNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
