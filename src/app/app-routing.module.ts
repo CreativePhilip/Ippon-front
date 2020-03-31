349e1a32b9bbf3a6dcfbab64a44d1ca902107950
Philip <creativepyt@gmail.com>
2020-04-01 00:05:15 +0200

    profile route and changes default font to nunito

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from "./pages/home/home.component"
 import { AuthenticationComponent } from "./pages/authentication/authentication.component"
+import {ProfileComponent} from "./pages/profile/profile/profile.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
-  { path: "login", component: AuthenticationComponent }
+  { path: "login", component: AuthenticationComponent },
+  { path: "profile", component: ProfileComponent },
 ];
 
 @NgModule({
