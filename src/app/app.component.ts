6342b0f7357d239e532be1197806d356b04e3520
Philip <creativepyt@gmail.com>
2020-04-03 01:33:36 +0200

    Setup google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import {AuthService} from "./services/auth-service/auth.service";
+import {NavigationEnd, Router} from "@angular/router";
+
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -9,7 +13,17 @@ import {AuthService} from "./services/auth-service/auth.service";
 export class AppComponent {
   title = 'Ippon';
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService,
+              public router: Router) {
     this.auth.loadTokensFromLocalStorage();
+
+
+    // Code responsible for sending google analytics events
+    // required because angular is a one page app
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        gtag('config', 'UA-162249928-1', {'page_path': event.urlAfterRedirects});
+      }
+    } )
   }
 }
